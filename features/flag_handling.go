e0718f726abc1c147b0becd52027fc730f7a730d
Hamza Maach <hamzamaach56@gmail.com>
2024-05-16 17:59:43 +0100

    Update: use map to store flags

--- features/flag_handling.go
+++ features/flag_handling.go
@@ -6,33 +6,56 @@ import (
 	"strings"
 )
 
-func ExtractOutputFlag(args []string) (map[string]string, []string) {
+func CheckOutputFlag(args []string) bool {
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "--output=") {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckColorFlag(args []string) bool {
+	for _, arg := range args {
+		if strings.HasPrefix(arg, "--color=") {
+			return true
+		}
+	}
+	return false
+}
+
+func ExtractFlags(args []string) (map[string]string, []string) {
 	flags := make(map[string]string)
 	var filteredArgs []string
+	var indexOfToBeColored int
 
 	for i, arg := range args {
 		if strings.HasPrefix(arg, "--output=") && i == 0 {
 			flags["outputFile"] = extractOutputFileName(arg)
 		} else if strings.HasPrefix(arg, "--color=") && i == 0 {
 			flags["color"] = extractColor(arg)
-			if flags["color"] != "" && len(args) == 4 {
-				flags["lettersToBeColored"] = args[i+1]
+			if flags["color"] != "" && i+2 < len(args) {
+				if AreLettersToBeColored(args[i+1], args[i+2]) {
+					flags["lettersToBeColored"] = args[i+1]
+					indexOfToBeColored = i + 1
+				}
 			}
 		} else if strings.HasPrefix(arg, "--") {
-			Usage()
+			Usage("color")
 			os.Exit(0)
 		} else {
-			filteredArgs = append(filteredArgs, arg)
+			if i != indexOfToBeColored {
+				filteredArgs = append(filteredArgs, arg)
+			}
 		}
 	}
-
 	return flags, filteredArgs
 }
 
 func extractOutputFileName(flag string) string {
 	fileName := strings.TrimPrefix(flag, "--output=")
 	if len(fileName) == 0 {
-		Usage()
+		Usage("output")
 		os.Exit(0)
 	}
 	if fileName != "" {
@@ -47,46 +70,8 @@ func extractOutputFileName(flag string) string {
 func extractColor(flag string) string {
 	color := strings.TrimPrefix(flag, "--color=")
 	if len(color) == 0 {
-		Usage()
+		Usage("color")
 		os.Exit(0)
 	}
 	return color
 }
-
-// func ExtractOutputFlag(args []string) (string, []string) {
-// 	var outputFile string
-// 	var filteredArgs []string
-
-// 	// check if arguments are given as one string
-// 	if len(args) == 1 {
-// 		tempArgs := strings.Fields(args[0])
-// 		for _, v := range tempArgs {
-// 			if strings.HasPrefix(v, "--") {
-// 				args = tempArgs
-// 			}
-// 		}
-// 	}
-
-// 	for i, arg := range args {
-// 		if strings.HasPrefix(arg, "--output=") && i == 0 {
-// 			outputFile = strings.TrimPrefix(arg, "--output=")
-// 			if len(outputFile) == 0 {
-// 				// fmt.Printf("you must include the output file!")
-// 				Usage()
-// 				os.Exit(0)
-// 			}
-// 		} else if strings.HasPrefix(arg, "--") {
-// 			Usage()
-// 			os.Exit(0)
-// 		} else {
-// 			filteredArgs = append(filteredArgs, arg)
-// 		}
-// 	}
-// 	if outputFile != "" {
-// 		if err := ValidateFileExtension(outputFile); err != nil {
-// 			fmt.Fprintln(os.Stderr, err)
-// 			os.Exit(0)
-// 		}
-// 	}
-// 	return outputFile, filteredArgs
-// }
