83c97bffeb9a4137a65981282153aa3400bcb274
Hamza Maach <hamzamaach56@gmail.com>
2024-05-28 18:50:12 +0100

    udpate variables name

--- features/asciiart_drawing.go
+++ features/asciiart_drawing.go
@@ -8,13 +8,13 @@ func DrawASCIIArt(
 	splittedInput []string,
 	hasNonEmptyLines bool,
 	flag map[string]string,
-	runesToBeColored []rune) string {
-
+	runesToBeColored []rune,
+) string {
 	var result strings.Builder
 	color := flag["color"]
 
-	for i, val := range splittedInput {
-		if val == "" {
+	for i, inputLine := range splittedInput {
+		if inputLine == "" {
 			if hasNonEmptyLines || i != 0 {
 				result.WriteString("\n")
 			}
@@ -22,18 +22,22 @@ func DrawASCIIArt(
 		}
 
 		// Find the starting indices of all occurrences of the substring to be colored
-		substringIndices := FindSubStringIndices(val, flag["lettersToBeColored"])
+		substringIndices := FindSubStringIndices(inputLine, flag["lettersToBeColored"])
 		substringLen := len(flag["lettersToBeColored"])
 
 		// Draw each character of the line in ASCII art format
-		for j := 0; j < 8; j++ {
-			for kIdx, k := range val {
+		for line := 0; line < 8; line++ {
+			for charIdx, char := range inputLine {
 				// Determine if the current character should be colorized
-				shouldColorize := substringLen == 0 || isInRange(kIdx, substringIndices, substringLen)
-				if color != "" && shouldColorize {
-					result.WriteString(Colorize(characterMatrix[k][j], color))
+				shouldColorize := substringLen == 0 || isInRange(charIdx, substringIndices, substringLen)
+				if color != "" && shouldColorize && char != 32 {
+					result.WriteString(Colorize(characterMatrix[char][line], color))
 				} else {
-					result.WriteString(characterMatrix[k][j])
+					if char == 32 && flag["align"] == "justify" {
+						result.WriteString("{space}")
+					} else {
+						result.WriteString(characterMatrix[char][line])
+					}
 				}
 			}
 			result.WriteString("\n")
